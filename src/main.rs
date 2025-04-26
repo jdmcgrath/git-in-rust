3f493428f67f4e6142904e90bf0b28f34c20e518
Joe McGrath <Joseph.mcgrath@aviva.com>
2025-04-27 00:58:37 +0100

    Add CatFile

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,162 @@
-#[allow(unused_imports)]
-use std::env;
-#[allow(unused_imports)]
+use anyhow::Context;
+use clap::{Parser, Subcommand};
+use flate2::read::ZlibDecoder;
+use std::ffi::CStr;
 use std::fs;
+use std::io::prelude::*;
+use std::io::BufReader;
 
-fn main() {
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+/// Doc comment
+#[derive(Debug, Subcommand)]
+enum Command {
+    Init,
+    CatFile {
+        #[clap(short = 'p')]
+        pretty_print: bool,
+        object_hash: String,
+    },
+}
+
+enum Kind {
+    Blob,
+}
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
     eprintln!("Logs from your program will appear here!");
+    // Uncomment this block to pass the first stage
 
-    let args: Vec<String> = env::args().collect();
-    match args[1].as_str() {
-        "init" => init(),
-        "cat-file" => cat_file(args),
-        _ => println!("unknown command: {}", args[1]),
+    match args.command {
+
+        Command::Init => {
+
+            fs::create_dir(".git").unwrap();
+
+            fs::create_dir(".git/objects").unwrap();
+
+            fs::create_dir(".git/refs").unwrap();
+
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+
+            println!("Initialized git directory")
+
+        }
+
+        Command::CatFile {
+
+            pretty_print,
+
+            object_hash,
+
+        } => {
+
+            anyhow::ensure!(
+
+                pretty_print,
+
+                "mode must be given without -p, and we don't support mode"
+
+            );
+
+            // TODO: support shortest-unique object hashes
+
+            let f = std::fs::File::open(format!(
+
+                ".git/objects/{}/{}",
+
+                &object_hash[..2],
+
+                &object_hash[2..]
+
+            ))
+
+            .context("open in .git/objects")?;
+
+            let z = ZlibDecoder::new(f);
+
+            let mut z = BufReader::new(z);
+
+            let mut buf = Vec::new();
+
+            z.read_until(0, &mut buf)
+
+                .context("read header from .git/objects")?;
+
+            let header = CStr::from_bytes_with_nul(&buf)
+
+                .expect("know there is exactly one nul, and it's at the end");
+
+            let header = header
+
+                .to_str()
+
+                .context(".git/objects file header isn't valid UTF-8")?;
+
+            let Some((kind, size)) = header.split_once(' ') else {
+
+                anyhow::bail!(
+
+                    ".git/objects file header did not start with a known type: '{header}'"
+
+                );
+
+            };
+
+            let kind = match kind {
+
+                "blob" => Kind::Blob,
+
+                _ => anyhow::bail!("we do not yet know how to print a '{kind}'"),
+
+            };
+
+            let size = size
+
+                .parse::<usize>()
+
+                .context(".git/objects file header has invalid size: {size}")?;
+
+            buf.clear();
+
+            buf.resize(size, 0);
+
+            z.read_exact(&mut buf[..])
+
+                .context("read true contents of .git/objects file")?;
+
+            let n = z
+
+                .read(&mut [0])
+
+                .context("validate EOF in .git/object file")?;
+
+            anyhow::ensure!(n == 0, ".git/object file had {n} trailing bytes");
+
+            let stdout = std::io::stdout();
+
+            let mut stdout = stdout.lock();
+
+            match kind {
+
+                Kind::Blob => stdout
+
+                    .write_all(&buf)
+
+                    .context("write object contents to stdout")?,
+
+            }
+
+        }
+
     }
-}
 
-fn init() {
-    fs::create_dir(".git").unwrap();
-    fs::create_dir(".git/objects").unwrap();
-    fs::create_dir(".git/refs").unwrap();
-    fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-    println!("Initialized git directory") 
-}
+    Ok(())
 
-fn cat_file(args: Vec<String>) {
-    if args[2] == "-p" {
-        let object_id = &args[3];
-        let object_path = format!(".git/objects/{}/{}", &object_id[0..2], &object_id[2..]);
-        let contents = fs::read_to_string(object_path).unwrap();
-        println!("{}", contents);
-    }
 }
