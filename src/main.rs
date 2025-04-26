f4b8a8fec7101c112a7d5b7c3618a710ae1327f4
Joe McGrath <Joseph.mcgrath@aviva.com>
2025-04-27 00:15:17 +0100

    Cleanup and add cat-file

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,29 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     eprintln!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+    match args[1].as_str() {
+        "init" => init(),
+        "cat-file" => cat_file(args),
+        _ => println!("unknown command: {}", args[1]),
+    }
+}
+
+fn init() {
+    fs::create_dir(".git").unwrap();
+    fs::create_dir(".git/objects").unwrap();
+    fs::create_dir(".git/refs").unwrap();
+    fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+    println!("Initialized git directory") 
+}
+
+fn cat_file(args: Vec<String>) {
+    if args[2] == "-p" {
+        let object_id = &args[3];
+        let object_path = format!(".git/objects/{}/{}", &object_id[0..2], &object_id[2..]);
+        let contents = fs::read_to_string(object_path).unwrap();
+        println!("{}", contents);
     }
 }
