54de615372bf811480b3f7198e135a04a0db92c5
Joe McGrath <Joseph.mcgrath@aviva.com>
2025-04-27 01:22:23 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@ struct Args {
     command: Command,
 }
 
-/// Doc comment
 #[derive(Debug, Subcommand)]
 enum Command {
     Init,
@@ -32,131 +31,72 @@ fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     eprintln!("Logs from your program will appear here!");
     // Uncomment this block to pass the first stage
-
     match args.command {
-
-        Command::Init => {
-
-            fs::create_dir(".git").unwrap();
-
-            fs::create_dir(".git/objects").unwrap();
-
-            fs::create_dir(".git/refs").unwrap();
-
-            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-
-            println!("Initialized git directory")
-
-        }
-
+        Command::Init => Ok(init()),
         Command::CatFile {
-
             pretty_print,
-
             object_hash,
-
-        } => {
-
-            anyhow::ensure!(
-
-                pretty_print,
-
-                "mode must be given without -p, and we don't support mode"
-
-            );
-
-            // TODO: support shortest-unique object hashes
-
-            let f = std::fs::File::open(format!(
-
-                ".git/objects/{}/{}",
-
-                &object_hash[..2],
-
-                &object_hash[2..]
-
-            ))
-
-            .context("open in .git/objects")?;
-
-            let z = ZlibDecoder::new(f);
-
-            let mut z = BufReader::new(z);
-
-            let mut buf = Vec::new();
-
-            z.read_until(0, &mut buf)
-
-                .context("read header from .git/objects")?;
-
-            let header = CStr::from_bytes_with_nul(&buf)
-
-                .expect("know there is exactly one nul, and it's at the end");
-
-            let header = header
-
-                .to_str()
-
-                .context(".git/objects file header isn't valid UTF-8")?;
-
-            let Some((kind, size)) = header.split_once(' ') else {
-
-                anyhow::bail!(
-
-                    ".git/objects file header did not start with a known type: '{header}'"
-
-                );
-
-            };
-
-            let kind = match kind {
-
-                "blob" => Kind::Blob,
-
-                _ => anyhow::bail!("we do not yet know how to print a '{kind}'"),
-
-            };
-
-            let size = size
-
-                .parse::<usize>()
-
-                .context(".git/objects file header has invalid size: {size}")?;
-
-            buf.clear();
-
-            buf.resize(size, 0);
-
-            z.read_exact(&mut buf[..])
-
-                .context("read true contents of .git/objects file")?;
-
-            let n = z
-
-                .read(&mut [0])
-
-                .context("validate EOF in .git/object file")?;
-
-            anyhow::ensure!(n == 0, ".git/object file had {n} trailing bytes");
-
-            let stdout = std::io::stdout();
-
-            let mut stdout = stdout.lock();
-
-            match kind {
-
-                Kind::Blob => stdout
-
-                    .write_all(&buf)
-
-                    .context("write object contents to stdout")?,
-
-            }
-
-        }
-
-    }
-
+        } => cat_file(pretty_print, object_hash),
+    }?;
     Ok(())
+}
 
+fn init() {
+    fs::create_dir(".git").unwrap();
+    fs::create_dir(".git/objects").unwrap();
+    fs::create_dir(".git/refs").unwrap();
+    fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+    println!("Initialized git directory") 
+}
+
+fn cat_file(pretty_print: bool, object_hash: String) -> anyhow::Result<()> {
+    anyhow::ensure!(
+        pretty_print,
+        "mode must be given without -p, and we don't support pretty print mode"
+    );
+
+    let f = std::fs::File::open(format!(
+        ".git/objects/{}/{}",
+        &object_hash[..2],
+        &object_hash[2..]
+    ))
+    .context("open in .git/objects")?;
+    let z = ZlibDecoder::new(f);
+    let mut z = BufReader::new(z);
+    let mut buf = Vec::new();
+    z.read_until(0, &mut buf)
+        .context("read header from .git/objects")?;
+    let header = CStr::from_bytes_with_nul(&buf)
+        .expect("know there is exactly one nul, and it's at the end");
+    let header = header
+        .to_str()
+        .context(".git/objects file header isn't valid UTF-8")?;
+    let Some((kind, size)) = header.split_once(' ') else {
+        anyhow::bail!(
+            ".git/objects file header did not start with a known type: '{header}'"
+        );
+    };
+    let kind = match kind {
+        "blob" => Kind::Blob,
+        _ => anyhow::bail!("we do not yet know how to print a '{kind}'"),
+    };
+    let size = size
+        .parse::<usize>()
+        .context(".git/objects file header has invalid size: {size}")?;
+    buf.clear();
+    buf.resize(size, 0);
+    z.read_exact(&mut buf[..])
+        .context("read true contents of .git/objects file")?;
+    let n = z
+        .read(&mut [0])
+        .context("validate EOF in .git/object file")?;
+    anyhow::ensure!(n == 0, ".git/object file had {n} trailing bytes");
+    let stdout = std::io::stdout();
+    let mut stdout = stdout.lock();
+    match kind {
+        Kind::Blob => stdout
+            .write_all(&buf)
+            .context("write object contents to stdout")?,
+    }
+    Ok(())
 }
